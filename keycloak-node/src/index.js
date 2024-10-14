f0c218e01b9cecfa792b30cde51f29ac15d06fc9
JulianRotti <julianrottenheimer1989@gmail.com>
2024-10-14 20:55:16 +0200

    add backend auth

--- keycloak-node/src/index.js
+++ keycloak-node/src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import AppRoutes from './routes/AppRoutes.js';
+import cors from 'cors';  // Import CORS middleware
 import { keycloak } from './middleware/authMiddleware.js';
 
 const app = express();
@@ -9,6 +10,11 @@ app.use(keycloak.middleware());
 
 // Middleware
 app.use(express.json());
+
+// Enable CORS for requests from http://localhost:3000 (React frontend)
+app.use(cors({
+    origin: 'http://localhost:3000',  // Allow only your frontend
+}));
 
 // Routes
 app.use('/api', AppRoutes);
